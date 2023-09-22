7d8b0ce322a4e32c758534bce4657fe791358626
Tony Santiago <85644672+t0nysantiago@users.noreply.github.com>
2023-09-22 16:58:37 -0300

    feat: building new chart

--- src/Modules/homeCras/controller/homeCras.controller.ts
+++ src/Modules/homeCras/controller/homeCras.controller.ts
@@ -16,8 +16,6 @@ export const HomeCrasController = () => {
         }
     }
 
-    
-
     return {
         services
     }
--- src/Modules/homeCras/services/homeCras.service.ts
+++ src/Modules/homeCras/services/homeCras.service.ts
@@ -12,12 +12,32 @@ export const HomeCrasServices = () => {
 
   const servicesResume = async (body: tecnicoAttributes) => {
     try {
-      const services = await connection.query(
-        "SELECT id, nome, qtd_atendimentos, total FROM `cras-db`.resumo_atendimentos;",
+
+      const result = [];
+
+      const qnt_atendimento = await connection.query(
+        "SELECT id, nome as name, qtd_atendimentos as value, total FROM `cras-db`.resumo_atendimentos;",
         { type: sequelize.QueryTypes.SELECT }
       );
 
-      return services;
+      // const bolsa_familia = await connection.query(
+      //   `SELECT "Pessoas sem bolsa familia" as name, SUM(CASE WHEN sf.bolsa_familia = 0 THEN 1 ELSE 0 END) as valor, COUNT(*) as total
+      //   FROM ${`cras-db.`}identificacao_usuario iu 
+      //   JOIN ${`cras-db.`}situacao_financeira sf ON sf.id = iu.id_situacao_financeira
+        
+      //   UNION
+        
+      //   SELECT "Pessoas com bolsa familia" as name, SUM(CASE WHEN sf.bolsa_familia > 0 THEN 1 ELSE 0 END) as valor, COUNT(*) as total
+      //   FROM ${`cras-db.`}identificacao_usuario iu 
+      //   JOIN ${`cras-db.`}situacao_financeira sf ON sf.id = iu.id_situacao_financeira
+      //   `,
+      //   { type: sequelize.QueryTypes.SELECT}
+      // );
+
+      result.push({title: "Atendimentos", data: qnt_atendimento});
+      // result.push({title: "Bolsa Familia", data: bolsa_familia});
+
+      return result;
     } catch (error) {
       const err: ErrorType = makeErrorMessage(`${error}`, 500);
       throw err;
