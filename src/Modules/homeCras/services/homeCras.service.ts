530f167c954fa56b7e16777ab90133c178f8a605
Tony Santiago <85644672+t0nysantiago@users.noreply.github.com>
2023-09-26 10:21:06 -0300

    feat: fixed query in sequelize

--- src/Modules/homeCras/services/homeCras.service.ts
+++ src/Modules/homeCras/services/homeCras.service.ts
@@ -12,7 +12,6 @@ export const HomeCrasServices = () => {
 
   const servicesResume = async (body: tecnicoAttributes) => {
     try {
-
       const result = [];
 
       const qnt_atendimento = await connection.query(
@@ -20,22 +19,27 @@ export const HomeCrasServices = () => {
         { type: sequelize.QueryTypes.SELECT }
       );
 
-      // const bolsa_familia = await connection.query(
-      //   `SELECT "Pessoas sem bolsa familia" as name, SUM(CASE WHEN sf.bolsa_familia = 0 THEN 1 ELSE 0 END) as valor, COUNT(*) as total
-      //   FROM ${`cras-db.`}identificacao_usuario iu 
-      //   JOIN ${`cras-db.`}situacao_financeira sf ON sf.id = iu.id_situacao_financeira
-        
-      //   UNION
-        
-      //   SELECT "Pessoas com bolsa familia" as name, SUM(CASE WHEN sf.bolsa_familia > 0 THEN 1 ELSE 0 END) as valor, COUNT(*) as total
-      //   FROM ${`cras-db.`}identificacao_usuario iu 
-      //   JOIN ${`cras-db.`}situacao_financeira sf ON sf.id = iu.id_situacao_financeira
-      //   `,
-      //   { type: sequelize.QueryTypes.SELECT}
-      // );
+      const bolsa_familia = await connection.query(
+        `
+    SELECT "Pessoas sem bolsa familia" as name, 
+           SUM(CASE WHEN sf.bolsa_familia = 0 THEN 1 ELSE 0 END) as value, 
+           COUNT(*) as total
+    FROM \`cras-db\`.identificacao_usuario iu 
+    JOIN \`cras-db\`.situacao_financeira sf ON sf.id = iu.id_situacao_financeira
+    
+    UNION
+    
+    SELECT "Pessoas com bolsa familia" as name, 
+           SUM(CASE WHEN sf.bolsa_familia > 0 THEN 1 ELSE 0 END) as value, 
+           COUNT(*) as total
+    FROM \`cras-db\`.identificacao_usuario iu 
+    JOIN \`cras-db\`.situacao_financeira sf ON sf.id = iu.id_situacao_financeira
+`,
+        { type: sequelize.QueryTypes.SELECT }
+      );
 
-      result.push({title: "Atendimentos", data: qnt_atendimento});
-      // result.push({title: "Bolsa Familia", data: bolsa_familia});
+      result.push({ title: "Atendimentos", data: qnt_atendimento });
+      result.push({ title: "Bolsa Familia", data: bolsa_familia });
 
       return result;
     } catch (error) {
