2a16484b09cf27195f7e11cfcd9fb15ae09a1228
Tony Santiago <85644672+t0nysantiago@users.noreply.github.com>
2023-09-26 16:39:54 -0300

    feat: changing cras to nest

--- src/util/error.services.ts
+++
@@ -1,9 +0,0 @@
-import { ErrorType } from "./error.type.js";
-
-export const makeErrorMessage = (message: string, code: number) => {
-    const error: ErrorType = {
-        code,
-        message,
-    };
-    return error;
-};
